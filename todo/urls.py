789a1ccaa71b1005a95d066d2fe97b6bd126c6bf
zerohyun <kyhh39@naver.com>
2023-05-24 12:39:54 +0900

    Todo Create기능 구현하기

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-
-from todo.views import todos
-
-
-
-
+from todo.views import create_todo, index, todos
 
 urlpatterns = [
-	path('', todos),
+	path('', todos, name="todos"),
+	path('index/', index),
+	path('create_todo/', create_todo, name="create"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from todo.models import TodoList
 
 # Create your views here.
+def create_todo(request):
+	myTodo = TodoList()
+	if request.method == 'POST':
+		myTodo.todo = request.POST['todo']
+		myTodo.description = request.POST['description']
+		myTodo.important = request.POST.get('important') == "on"
+		myTodo.complete = request.POST.get('complete') == "on"
+		myTodo.save()
+		return redirect('todos')
+	return render(
+		request,
+		'todo/todo_create.html'
+	)
 def todos(request):
 	todolist = TodoList.objects.all()
 	return render(
@@ -10,4 +23,15 @@ def todos(request):
   		{
 			'todolist': todolist,
 			}
+	)
+def index(request):
+	#todolist의 모든 레코드를 가져온다.
+	todolist = TodoList.objects.all()
+	return render(
+		request,
+		'todo/index.html',
+		{
+			#HTML에서 todolist라는 이름으로 todolist를 넘겨준다.
+			'todolist': todolist,
+		}
 	)
