2c22283c6279aadbcad9130e800a6cfc8e4a9133
zerohyun <kyhh39@naver.com>
2023-07-04 15:11:06 +0900

    create UserManager

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,31 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, AbstractUser
 )
-
+class UserManager(BaseUserManager) :
+    def create_user(self, email, password, staff=False, admin=False, active=True):
+        if not email:
+            raise ValueError('이메일을 입력해주세요!')
+        if not password:
+            raise ValueError('비밀번호를 입력해주세요!')
+    
+        user = self.model( email = self.normalize_email(email))
+        user.set_password(password) # set_password()는 어떤 함수?
+        user.staff = staff
+        user.admin = admin
+        user.active = active
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password,
+            staff = True,
+            admin = True
+        )
+        return user
+    
 class User(AbstractBaseUser) :
     email = models.EmailField(max_length=255, unique=True)
     nickname = models.CharField(max_length=255, blank=True, null=True)
@@ -12,16 +36,21 @@ class User(AbstractBaseUser) :
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    
-def __str__(self):
-    return self.email
+    objects = UserManager()
 
-@property
-def is_staff(self):
-    return self.staff
-@property
-def is_superuser(self):
-    return self.admin
+    def __str__(self):
+        return self.email
 
+    @property
+    def is_staff(self):
+        return self.staff
+    @property
+    def is_superuser(self):
+        return self.admin
+
+    def has_perm(self, perm, obj=None) :
+        return self.admin
+    def has_module_perms(self, app_label):
+        return self.admin
 
 # Create your models here.
